566c0d3a0e24922d29bead4c3ed2dc0ef39624d1
yonicb <yonicb20@gmail.com>
2019-12-29 18:32:09 -0500

    feat: 🎸 Main test in this file

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+/*!
+ * Cbytes v1.0.0 (https://github.com/yonicb/cbytes)
+ * Copyright 2019 The Cbytes Authors
+ * Copyright 2019 Yoni Calsin <@yonicb>.
+ * Licensed under MIT (https://github.com/yonicb/cbytes/blob/master/LICENSE)
+ */
+const hello = (name: string) => {
+    console.log(`Hello ${name}`);
+};
+
+hello('Yoni Calsin');
